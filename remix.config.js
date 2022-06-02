c6cf5917817d8da4310b22d5be390025e2c1b81b
MohamedHakem <mohamedhakem959628@gmail.com>
2022-06-02 05:19:40 +0200

    deploy to vercel

--- remix.config.js
+++ remix.config.js
@@ -1,22 +1,11 @@
-// /**
-//  * @type {import('@remix-run/dev').AppConfig}
-//  */
-// module.exports = {
-//   cacheDirectory: "./node_modules/.cache/remix",
-//   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-//   serverDependenciesToBundle: [
-//     "marked",
-//   ],
-// };
-
-// from/for deploying to vercel
 /**
- * @type {import('@remix-run/dev/config').AppConfig}
+ * @type {import('@remix-run/dev').AppConfig}
  */
- module.exports = {
-  appDirectory: "app",
-  browserBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "api/_build",  
+module.exports = {
+  cacheDirectory: "./node_modules/.cache/remix",
+  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverDependenciesToBundle: [
+    "marked",
+  ], 
   serverBuildTarget: "vercel"
 };
